41a9f4192638bee5fa40dd4e21fa41d4ef8d2214
Joel Loo <j2loo97@gmail.com>
2020-04-30 17:52:31 -0700

    testing scrollTo

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+function scrollToAnchor(aid){
+  	  var aTag = $(“#”+ aid);
+  	  $(‘body’).css({
+  	  	overflow: ‘initial’
+  	  });
+  	  $(‘html,body’).animate({scrollTop: aTag.offset().top},‘slow’).promise().then(
+  	  		function() {
+  	  			$(‘body’).css({
+  	    	  	overflow: ‘hidden’
+  	    	  });
+  	  		}
+  	  );
+}
+
+$(“.scrollTo”).click(function() {	
+   	 scrollToAnchor($(this).data(‘href’));
+});
